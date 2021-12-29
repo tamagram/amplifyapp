7876f439f86de9d2337eadc27cd6a480284c3787
tamagram <tamadevelopment211@gmail.com>
2021-12-29 21:00:38 +0900

    :sparkles: Implement delete and update.

--- src/components/SkuTable.jsx
+++ src/components/SkuTable.jsx
@@ -14,6 +14,10 @@ import {
 import { useState, useEffect, useContext } from "react";
 import { API } from "aws-amplify";
 import { listProducts as listProductsQuery } from "../graphql/queries";
+import {
+  updateProduct as updateProductMutation,
+  deleteProduct as deleteProductMutation,
+} from "../graphql/mutations";
 
 const SkuTable = () => {
   const [dbMessage, setDbMessage] = useState("Loading...");
@@ -22,12 +26,15 @@ const SkuTable = () => {
   const [products, setProducts] = useState([]);
   const [show, setShow] = useState(false);
 
+  const [showEdit, setShowEdit] = useState(false);
+  const [editingTarget, setEditingTarget] = useState({});
+
   const [name, setName] = useState("");
   const [price, setPrice] = useState(0);
   const [size, setSize] = useState("");
 
   const [brandCode, setBrandCode] = useState("");
-  const [years, setYears] = useState("");
+  const [year, setYear] = useState("");
   const [season, setSeason] = useState("");
   const [largeCategory, setLargeCategory] = useState("");
   const [mediumCategory, setMediumCategory] = useState("");
@@ -61,9 +68,9 @@ const SkuTable = () => {
         eq: brandCode,
       };
     }
-    if (years) {
+    if (year) {
       filter.year = {
-        eq: years,
+        eq: year,
       };
     }
     if (season) {
@@ -98,7 +105,7 @@ const SkuTable = () => {
     price,
     size,
     brandCode,
-    years,
+    year,
     season,
     largeCategory,
     mediumCategory,
@@ -140,8 +147,44 @@ const SkuTable = () => {
       });
   };
 
+  const updateProduct = () => {
+    API.graphql({
+      query: updateProductMutation,
+      variables: {
+        input: editingTarget,
+      },
+    })
+      .then(() => {
+        setShowEdit(false);
+        alert("更新しました");
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("更新に失敗しました");
+      });
+  };
+
+  const deleteProduct = () => {
+    API.graphql({
+      query: deleteProductMutation,
+      variables: {
+        input: {
+          id: editingTarget.id,
+        },
+      },
+    })
+      .then(() => {
+        alert("削除しました");
+        fetchProducts();
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("削除に失敗しました");
+      });
+  };
+
   const productsLi = products.map((product) => (
-    <tr key={product.sku}>
+    <tr key={product.id}>
       <td>{product.name}</td>
       <td>{product.price || "未設定"}</td>
       <td>{product.size || "未設定"}</td>
@@ -153,6 +196,31 @@ const SkuTable = () => {
       <td>{product.smallCategory}</td>
       <td>{product.color}</td>
       <td>{product.sku}</td>
+      <td>
+        <Button
+          variant="secondary"
+          size="sm"
+          onClick={() => {
+            setShowEdit(true);
+            setEditingTarget({
+              id: product.id,
+              name: product.name,
+              price: product.price,
+              size: product.size,
+              brandCode: product.brandCode,
+              year: product.year,
+              season: product.season,
+              largeCategory: product.largeCategory,
+              mediumCategory: product.mediumCategory,
+              smallCategory: product.smallCategory,
+              color: product.color,
+              sku: product.sku,
+            });
+          }}
+        >
+          編集
+        </Button>
+      </td>
     </tr>
   ));
   return (
@@ -193,9 +261,9 @@ const SkuTable = () => {
                       <Form.Control
                         required
                         type="text"
-                        value={years}
+                        value={year}
                         onChange={(e) => {
-                          setYears(e.target.value);
+                          setYear(e.target.value);
                         }}
                       />
                     </Col>
@@ -338,12 +406,312 @@ const SkuTable = () => {
                   <th>小カテゴリ</th>
                   <th>カラー</th>
                   <th>SKU</th>
+                  <th></th>
                 </tr>
               </thead>
               <tbody>{productsLi}</tbody>
             </Table>
           </Col>
         </Row>
+        <Modal size="lg" show={showEdit} onHide={() => setShowEdit(false)}>
+          <Container>
+            <Row>
+              <Col>
+                <Card className="my-3">
+                  <Stack gap={2} className="m-3">
+                    <Form.Group as={Row} controlId="validationCustom01">
+                      <Form.Label column sm="5">
+                        ブランドコード
+                      </Form.Label>
+                      <Col>
+                        <Form.Control
+                          required
+                          type="string"
+                          value={editingTarget.brandCode}
+                          onChange={(e) => {
+                            setEditingTarget({
+                              ...editingTarget,
+                              brandCode: e.target.value,
+                            });
+                          }}
+                        />
+                      </Col>
+                      <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group as={Row} controlId="validationCustom01">
+                      <Form.Label column sm="5">
+                        年数
+                      </Form.Label>
+                      <Col>
+                        <Form.Control
+                          required
+                          type="text"
+                          value={editingTarget.year}
+                          onChange={(e) => {
+                            setEditingTarget({
+                              ...editingTarget,
+                              year: e.target.value,
+                            });
+                          }}
+                        />
+                      </Col>
+                      <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                    </Form.Group>
+                    <FloatingLabel controlId="floatingSelect" label="シーズン">
+                      <Form.Select
+                        aria-label="Floating label select example"
+                        value={editingTarget.season}
+                        onChange={(e) => {
+                          setEditingTarget({
+                            ...editingTarget,
+                            season: e.target.value,
+                          });
+                        }}
+                      >
+                        <option>選択</option>
+                        <option value="1">SS : 1</option>
+                        <option value="3">AW : 3</option>
+                      </Form.Select>
+                    </FloatingLabel>
+                    <FloatingLabel
+                      controlId="floatingSelect"
+                      label="大カテゴリ"
+                    >
+                      <Form.Select
+                        aria-label="Floating label select example"
+                        value={editingTarget.largeCategory}
+                        onChange={(e) => {
+                          setEditingTarget({
+                            ...editingTarget,
+                            largeCategory: e.target.value,
+                          });
+                        }}
+                      >
+                        <option>選択</option>
+                        <option value="1">トップス : 1</option>
+                        <option value="2">ボトムス : 2</option>
+                        <option value="3">ワンピース、セットアップ : 3</option>
+                        <option value="4">アクセサリー : 4</option>
+                        <option value="5">シューズ : 5</option>
+                        <option value="6">アウター : 6</option>
+                      </Form.Select>
+                    </FloatingLabel>
+                    <FloatingLabel
+                      controlId="floatingSelect"
+                      label="中カテゴリ"
+                    >
+                      <Form.Select
+                        aria-label="Floating label select example"
+                        value={editingTarget.mediumCategory}
+                        onChange={(e) => {
+                          // setMediumCategory(e.target.value);
+                          setEditingTarget({
+                            ...editingTarget,
+                            mediumCategory: e.target.value,
+                          });
+                        }}
+                      >
+                        <option>選択</option>
+                        <option>◆トップス◆</option>
+                        <option value="11">Tシャツ : 11</option>
+                        <option value="12">シャツ : 12</option>
+                        <option value="13">ニット : 13</option>
+                        <option value="14">カーディガン : 14</option>
+                        <option value="15">ベスト : 15</option>
+                        <option value="16">スウェット : 16</option>
+                        <option value="17">フーディ : 17</option>
+                        <option>◆ボトムス◆</option>
+                        <option value="21">スラックス : 21</option>
+                        <option value="22">パンツ : 22</option>
+                        <option>◆セットアップ◆</option>
+                        <option value="31">セットアップ : 31</option>
+                        <option value="32">ジャンプスーツ : 32</option>
+                        <option>◆アクセサリー◆</option>
+                        <option value="41">ベルト : 41</option>
+                        <option value="42">バッグ : 42</option>
+                        <option value="43">ジュエリー : 43</option>
+                        <option value="44">アクセサリー その他 : 44</option>
+                        <option>◆シューズ◆</option>
+                        <option value="51">ブーツ : 51</option>
+                        <option value="52">スニーカー : 52</option>
+                        <option value="53">シューズ その他 : 53</option>
+                        <option>◆アウター◆</option>
+                        <option value="61">コート : 61</option>
+                        <option value="62">ブルゾン : 62</option>
+                        <option value="63">ジャケット : 63</option>
+                        <option value="64">アウター その他 : 64</option>
+                      </Form.Select>
+                    </FloatingLabel>
+                    <Form.Group as={Row} controlId="validationCustom01">
+                      <Form.Label column sm="5">
+                        小カテゴリ - フリーコード
+                      </Form.Label>
+                      <Col>
+                        <Form.Control
+                          required
+                          type="text"
+                          value={editingTarget.smallCategory}
+                          defaultValue="xx"
+                          onChange={(e) => {
+                            // setSmallCategory(e.target.value);
+                            setEditingTarget({
+                              ...editingTarget,
+                              smallCategory: e.target.value,
+                            });
+                          }}
+                        />
+                      </Col>
+                      <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                    </Form.Group>
+                    <FloatingLabel
+                      controlId="floatingSelect"
+                      label="カラー番号"
+                    >
+                      <Form.Select
+                        aria-label="Floating label select example"
+                        value={editingTarget.color}
+                        onChange={(e) => {
+                          setEditingTarget({
+                            ...editingTarget,
+                            color: e.target.value,
+                          });
+                        }}
+                      >
+                        <option>選択</option>
+                        <option value="100">WHT : 100</option>
+                        <option value="001">BLK : 001</option>
+                        <option value="200">NVY : 200</option>
+                        <option value="300">BGE : 300 (CREAM)</option>
+                        <option value="400">KHK : 400</option>
+                        <option value="500">GRY : 500</option>
+                        <option value="550">L/GRY : 550</option>
+                        <option value="660">GRN : 600</option>
+                        <option value="700">YLW : 700</option>
+                        <option value="800">RED : 800</option>
+                        <option value="900">BLU : 900</option>
+                        <option value="050">PPL : 050</option>
+                        <option value="111">GLD : 111</option>
+                        <option value="222">SLV : 222</option>
+                        <option value="999">MIX : 999</option>
+                        <option value="010">BRN : 010 (B/BLK)</option>
+                        <option value="000">DENIM : 000</option>
+                      </Form.Select>
+                    </FloatingLabel>
+                  </Stack>
+                </Card>
+              </Col>
+              <Col>
+                <Stack gap={2} className="m-3">
+                  <Form.Group as={Row} controlId="validationCustom01">
+                    <Form.Label column sm="5">
+                      商品名
+                    </Form.Label>
+                    <Col>
+                      <Form.Control
+                        required
+                        type="text"
+                        value={editingTarget.name}
+                        onChange={(e) => {
+                          setEditingTarget({
+                            ...editingTarget,
+                            name: e.target.value,
+                          });
+                        }}
+                      />
+                    </Col>
+                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                  </Form.Group>
+                  <Form.Group as={Row} controlId="validationCustom01">
+                    <Form.Label column sm="5">
+                      価格
+                    </Form.Label>
+                    <Col>
+                      <Form.Control
+                        required
+                        type="number"
+                        value={editingTarget.price}
+                        onChange={(e) => {
+                          setEditingTarget({
+                            ...editingTarget,
+                            price: e.target.value,
+                          });
+                        }}
+                      />
+                    </Col>
+                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                  </Form.Group>
+                  <Form.Group as={Row} controlId="validationCustom01">
+                    <Form.Label column sm="5">
+                      サイズ
+                    </Form.Label>
+                    <Col>
+                      <Form.Control
+                        required
+                        type="text"
+                        value={editingTarget.size}
+                        onChange={(e) => {
+                          setEditingTarget({
+                            ...editingTarget,
+                            size: e.target.value,
+                          });
+                        }}
+                      />
+                    </Col>
+                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                  </Form.Group>
+                  <Form.Group as={Row} controlId="formPlaintextEmail">
+                    <Form.Label column sm="5">
+                      SKU
+                    </Form.Label>
+                    <Col>
+                      <Form.Control
+                        type="text"
+                        readOnly
+                        value={editingTarget.sku}
+                      />
+                    </Col>
+                  </Form.Group>
+                  <Button
+                    md="8"
+                    variant="primary"
+                    onClick={async () => {
+                      setEditingTarget({
+                        ...editingTarget,
+                        sku:
+                          editingTarget.brandCode +
+                          editingTarget.year +
+                          editingTarget.season +
+                          editingTarget.largeCategory +
+                          editingTarget.mediumCategory +
+                          editingTarget.smallCategory +
+                          "-" +
+                          editingTarget.color,
+                      });
+
+                      if (window.confirm("更新しますか？")) {
+                        console.dir(editingTarget);
+                        updateProduct();
+                      }
+                    }}
+                  >
+                    更新
+                  </Button>
+                  <Button
+                    variant="danger"
+                    onClick={() => {
+                      if (window.confirm("本当に削除しますか？")) {
+                        console.dir(editingTarget);
+                        deleteProduct();
+                      }
+                    }}
+                  >
+                    削除
+                  </Button>
+                </Stack>
+              </Col>
+            </Row>
+          </Container>
+        </Modal>
       </Container>
     </>
   );
